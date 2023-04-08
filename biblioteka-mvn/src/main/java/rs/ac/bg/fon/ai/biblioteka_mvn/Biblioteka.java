9f68317650323e01a24827671c918157e73d0b34
Stojan123 <milosevicdejan65@gmail.com>
2023-04-08 18:20:48 +0200

    Napravljen maven projekat biblioteka-mvn

---
+++ biblioteka-mvn/src/main/java/rs/ac/bg/fon/ai/biblioteka_mvn/Biblioteka.java
@@ -0,0 +1,63 @@
+package rs.ac.bg.fon.ai.biblioteka_mvn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rs.ac.bg.fon.ai.biblioteka_mvn.interfejs.BibliotekaInterfejs;
+
+
+/**
+ * Implementira biblioteku, pri cemu je
+ * lista sa knjigama implementirana preko ArrayList klase
+ * 
+ * @author Bojan Tomic
+ * @since 0.9.0
+ */
+public class Biblioteka implements BibliotekaInterfejs {
+
+	/**
+	 * Lista sa knjigama iz biblioteke - ArrayList implementacija
+	 * 
+	 * @see biblioteka.Knjiga
+	 */
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+	
+	
+	//Pravi komentar, pravi kod
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga je duplikat");
+		
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if (autor == null && isbn < 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate uneti bar neki kriterijum za pretragu");
+		
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		
+		if (naslov!=null)
+		  for (Knjiga k: knjige)
+			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
+				rezultati.add(k);		
+		
+		return rezultati;
+	}
+
+}
